2afafcb28384e7560f383bb2fdc2c8a377740e5f
jasonoh428 <ohjason794@gmail.com>
2021-01-17 11:24:54 -0500

    update for Bitcoin Core 0.21 release Fixes #50

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,7 +4,7 @@ function TopBar () {
   return (
     <div className='mdl-layout__header'>
       <div className='mdl-layout__header-row'>
-        <span className='mdl-layout-title'>Bitcoin Core Config Generator (compatible with Bitcoin Core 0.20)</span>
+        <span className='mdl-layout-title'>Bitcoin Core Config Generator (compatible with Bitcoin Core 0.21)</span>
         <div className='mdl-layout-spacer' />
       </div>
     </div>
